0e3a2c009676af39958e21668870ee7fbc269baa
shao_yong <henry934629@gmail.com>
2023-04-21 10:06:29 +0800

    add testing

---
+++ Testing/UVa10420.py
@@ -0,0 +1,16 @@
+# UVa10420
+
+n = int(input())
+counties = {}
+
+for i in range(n):
+    string = list(map(str ,input().split(' ')))
+    if string[0] in counties:
+        counties[string[0]] += 1
+    else:
+        counties[string[0]] = 0
+
+ans = sorted(counties)
+
+for i in ans:
+    print(i, counties[i])
