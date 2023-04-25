494df3d959d63de09890f32c8801f7ad944ccf12
shao_yong <henry934629@gmail.com>
2023-04-25 14:30:33 +0800

    add testing files

--- Testing/UVa100.py
+++ Testing/UVa100.py
@@ -1,18 +1,31 @@
 def algorithm(x):
-    output = []
+    output = [x]
     
     while True:
-        print(f'{output =}')
+        
         if x == 1: break 
         if x % 2:
             x = 3*x + 1
         else:
             x //= 2
             output.append(x)
-        return output
+    # print(f'{output =}')
+    return output
 
-i, j = map(int, input().split())
-print(f'{i =}{j =}')
-i_list = algorithm(i)
-j_list = algorithm(j)
-print(f'{i_list =}{j_list =}')
+try:
+        
+    while True:    
+        i, j = map(int, input().split())
+        # print(f'{i =}\n{j =}')
+        i_list = algorithm(i)
+        j_list = algorithm(j)        
+        print(f'{i_list =}\n{j_list =}')
+        # print(len(i_list), len(j_list))
+        x = 0
+        for i in range(len(j_list), 1, -1):
+            x += i
+            print(f'{i = }\n{x = }')
+
+
+except EOFError:
+    pass
