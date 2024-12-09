5cc327d7369c934bb6f639c57929c50cd4f746c1
shao yong <henry934629@gmail.com>
2024-12-09 09:13:57 +0800

    Add UVa11078 to AC files.

---
+++ UVa_cpp/UVa11078.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int t, n;
+    cin >> t;
+    while(t --){
+        int ans = - (1 << 31);
+        int maxium, num;
+        cin >> n;
+        cin >> maxium;
+        for(int i=1; i<n; i++){
+            cin >> num;
+            // cout << num << "/" << maxium << '\n';
+            if ( maxium - num > ans ) {
+                ans = maxium - num;
+                // cout << "ans = " << ans << "\n";
+            }
+            if ( num > maxium ) maxium = num;
+        }
+        
+        cout << ans << "\n";
+    }
